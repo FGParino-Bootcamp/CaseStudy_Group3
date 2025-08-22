ac189ccb8da3becf4849aaaeeab1c3e4ad339fce
FG Parino <fritz.g.s.parino@accenture.com>
2025-08-22 13:58:22 +0000

    Revision 2

--- webapp/controller/MainPage.controller.js
+++ webapp/controller/MainPage.controller.js
@@ -3,11 +3,10 @@ sap.ui.define([
     'sap/ui/model/json/JSONModel',
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
-    'sap/ui/core/UIComponent',
-    "../model/formatter"
-], function (Controller, JSONModel, Filter, FilterOperator, Component, Formatter) {
+    '../model/formatter',
+    'sap/m/MessageBox'
+], function (Controller, JSONModel, Filter, FilterOperator, Formatter, MessageBox) {
         "use strict";
-
         return Controller.extend("casestudy.training.casestudyg3.controller.MainPage", {
             formatter:Formatter,
             onInit: function() {   
@@ -18,7 +17,12 @@ sap.ui.define([
                     if (oCount) {
                     oCount.setText("Orders(" + iCount + ")");
                     }
-                }.bind(this)); 
+                }.bind(this));  
+                var globalVar = new JSONModel({ 
+                    actionDelete: false,
+                    text:         ""
+            });
+                this.getView().setModel(globalVar, "FragmentData");      
             },
             onSearch: function () {
                 let oView  = this.getView();
@@ -72,8 +76,78 @@ sap.ui.define([
             },
             onAddOrder: function () {
                 //Navigate to Create Page
-                var oRouter = this.getOwnerComponent().getRouter();
+                let oRouter = this.getOwnerComponent().getRouter();
                 oRouter.navTo("RouteCreatePage");            
+            },
+            onSelectOrder: function (oEvent) {
+                let oSelectedItem= oEvent.getSource();
+                if (oSelectedItem) {
+                    var oSelectedObject = oSelectedItem.getBindingContext().getObject();
+                    var sSelectedKey = oSelectedObject.OrderID;
+                }
+                //Navigate to Display Page
+                let oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("RouteDisplayPage", { OrderID: sSelectedKey });
+            },
+            onDeleteOrder: function () {
+                let oView  = this.getView();
+                let oTable = oView.byId("OrderTable");
+                let oSelectedItems = oTable.getSelectedItems();
+                if (oSelectedItems.length === 0) {
+                    MessageBox.show("Please select an item from the table.",
+                        {
+                            icon: MessageBox.Icon.ERROR,
+                            title: "Error",
+                            actions: [MessageBox.Action.OK], // Optional: Define actions (e.g., OK, YES, NO)
+                        } );
+
+                } else {
+                    let sFragmentData = this.getView().getModel("FragmentData");
+                    sFragmentData.setProperty("/actionDelete", true);
+                    this.onConfirm(oSelectedItems.length, sFragmentData); 
+                }
+
+            },
+            onConfirm: function (count, fragment) {  
+                let sAction = fragment.getProperty("/actionDelete");
+                if (sAction) {
+                    fragment.setProperty("/text", "Are you sure you want to delete " + count + " item(s)?");              
+                }
+                if (!this.oConfDialog) {
+                    this.oConfDialog = this.loadFragment({
+                        name: "casestudy.training.casestudyg3.fragment.ConfirmationDialog"
+                    });
+                }
+                this.oConfDialog.then(function(oConfirmationDelete) {
+                    oConfirmationDelete.open();
+                });
+            },
+
+            onYes: function (oEvent) {
+                let oView  = this.getView();
+                let oTable = oView.byId("OrderTable");
+                var oModel = this.getOwnerComponent().getModel();
+                let oSelectedItems = oTable.getSelectedItems();
+                if (oSelectedItems.length > 0) {
+
+                    oSelectedItems.forEach(function(oItem) {
+                        let sPath = oItem.getBindingContextPath();
+                        
+                        oModel.remove(sPath, {  
+                                        success: function (oData, oResponse) {
+                                            oModel.refresh(true);
+                                        },
+                                        error: function (oError){
+                                        }
+                                    });
+                    });
+                }
+                this.getView().byId("idConfirmationDialog").close();
+            },
+
+            onNo: function () {
+                this.getView().byId("idConfirmationDialog").close();
             }
+
         });
     });
