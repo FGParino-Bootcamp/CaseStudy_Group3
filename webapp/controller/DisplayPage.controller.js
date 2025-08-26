1e7f50f975933120566f84ba03c52138ce9e0296
Mark Bryan Mabolo <mark.bryan.mabolo@accenture.com>
2025-08-26 13:15:27 +0000

    revision2_display

--- webapp/controller/DisplayPage.controller.js
+++ webapp/controller/DisplayPage.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     'sap/ui/model/json/JSONModel',
-    'sap/ui/core/UIComponent'
+    'sap/ui/core/UIComponent',
 ], function (Controller, JSONModel, Component) {
         "use strict";
 
@@ -10,9 +10,44 @@ sap.ui.define([
                 var oRouter = this.getOwnerComponent().getRouter();
                 oRouter.getRoute("RouteDisplayPage").attachPatternMatched(this._onObjectMatched, this);
             },
+
             _onObjectMatched: function(oEvent) {
-                var aArgs = oEvent.getParameter("arguments");
-                //aArgs.OrderID use this
+            var sOrderId = oEvent.getParameter("arguments").OrderID;
+        
+            var oModel = this.getView().getModel();
+            var opanel1 = this.getView().byId("idDisplayViewPanelOrderDetail");
+            var sReadUri = oModel.createKey("/Orders",
+                        {
+                            OrderID: sOrderId
+                        }
+                    );
+            oModel.read(sReadUri,{
+                success: function (oData) {
+                    if (oData) {
+                    
+                        var oOrderModel = new JSONModel(oData);
+                        opanel1.setModel(oOrderModel, "OrderData");
+                    }
+                },
+                error: function (oError){
+                    console.error("Error reading data:", oError);
+                }
+            })
+
+                var oTab = this.getView().byId("tabOrd");
+                var oBinding = oTab.getBinding("items");
+                var oFilter = new sap.ui.model.Filter("OrderID", sap.ui.model.FilterOperator.EQ, sOrderId);
+                oBinding.filter([oFilter]); 
+
             },
+
+            onEdit: function(){
+                this.getOwnerComponent().getRouter().navTo("RouteEditPage"); 
+
+            },
+
+            onCancel: function () {
+                this.getOwnerComponent().getRouter().navTo("RouteMainPage");
+        }
         });
     });
